c900f2cfaaa31e7704d5efc4277145e5760293d0
Multitracktools <multitracklivedirector@gmail.com>
2026-09-04 12:10:06 -0300

    refactor: adicionar wrapper do NDI

---
+++ src/ndi_sender.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <cstdint>
+#include <filesystem>
+#include <string>
+
+class NdiSender {
+public:
+    NdiSender() = default;
+    ~NdiSender();
+    NdiSender(const NdiSender&) = delete;
+    NdiSender& operator=(const NdiSender&) = delete;
+
+    bool open(const std::filesystem::path& runtimeDll, const std::string& sourceName, std::wstring& error);
+    void close();
+    bool sendFrame(const std::uint8_t* data, int width, int height, int fps);
+    int connections() const;
+    bool valid() const;
+
+private:
+    void* module_{};
+    const void* api_{};
+    void* sender_{};
+};
