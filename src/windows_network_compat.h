727f276641024448b96b5f53a66f93bf923f8bbb
Multitracktools <multitracklivedirector@gmail.com>
2026-09-04 12:00:43 -0300

    fix: evitar conflito de NTSTATUS em netioapi

--- src/windows_network_compat.h
+++ src/windows_network_compat.h
@@ -1,4 +1,12 @@
 #pragma once
 
 #include <windows.h>
+
+// O Windows SDK pode tentar redeclarar NTSTATUS ao incluir netioapi.h
+// depois de windows.h. Marcamos a definição como já disponível para evitar
+// o conflito, mantendo GetIfTable2/MIB_IF_TABLE2 acessíveis.
+#ifndef _NTDEF_
+#define _NTDEF_
+#endif
+
 #include <netioapi.h>
